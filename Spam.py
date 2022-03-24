f5a52c2bdd246a35bf3a52352c045e86c94b5e00
GamerTwarit <80262192+GamerTwarit@users.noreply.github.com>
2022-03-24 09:59:15 +0530

    Update Spam.py

--- Spam.py
+++ Spam.py
@@ -1,16 +1,8 @@
 import pyautogui as spam
 import time
 time.sleep(2)
- BLACK   = '\033[30m'
-    RED     = '\033[31m'
-    GREEN   = '\033[32m'
-    YELLOW  = '\033[33m'
-    BLUE    = '\033[34m'
-    MAGENTA = '\033[35m'
-    CYAN    = '\033[36m'
-    WHITE   = '\033[37m'
-    RESET   = '\033[39m'
-print(BLUE +'''    welcome to spam-PyAutoGUI 
+
+print('''    welcome to spam-PyAutoGUI 
 
 
 
@@ -25,7 +17,7 @@ print(BLUE +'''    welcome to spam-PyAutoGUI
  ''')
 input("press enter to start ")
 
-A = input(GREEN + "type the text you want to spam ")
+A = input("type the text you want to spam ")
 print("your text will look like", A)
 input("press enter to start")
 while True:
